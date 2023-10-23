f9f54f198c6fe89f56c09780dd5c148083f46d50
Oleg <olyubichenko@gmail.com>
2023-10-23 18:51:15 +0300

    first commit

---
+++ types/script.js
@@ -0,0 +1,23 @@
+let numb = 5;
+
+let str = 'fdhjhgkj';
+
+const obj = {
+	name: 'John',
+	age: 26,
+	isMaried: false,
+};
+
+let arr = ['plum', 'orange', 6, 'apple', {}, []];
+
+const storeName = "Shop1";
+
+const storeDescription = {
+    budget: 10000,
+    employees: ["John", "Marry", "Harry"],
+    products: {
+        apple: 3,
+        orange: 5,
+    },
+    open: true,
+};
---
+++ vars/script.js
@@ -0,0 +1,12 @@
+let number = 5;
+
+const borderWidth = 100;
+
+var x =  35;
+
+console.log(borderWidth);
+
+let userName = "John";
+let userNumber = 25;
+
+userNumber = 24;
